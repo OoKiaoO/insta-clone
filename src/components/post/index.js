2e93ce95f114a31db79f446662075f095243713a
Chiara Campalani <campalanichiara@gmail.com>
2021-05-09 18:22:04 +0900

    created post header component

---
+++ src/components/post/index.js
@@ -0,0 +1,26 @@
+import { useRef } from 'react';
+import PropTypes from 'prop-types';
+import Header from './header';
+
+export default function Post({ content }) {
+  // components:
+  // header, photo, tions (like & comment icons), footer, comment box
+  return (
+    <div className="rounded col-span-4 bg-white border border-gray-primary mb-16">
+      <Header username={content.username} />
+    </div>
+  );
+}
+
+Post.propTypes = {
+  content: PropTypes.shape({
+    username: PropTypes.string.isRequired,
+    imageSrc: PropTypes.string.isRequired,
+    caption: PropTypes.string.isRequired,
+    docId: PropTypes.string.isRequired,
+    userLikedPhoto: PropTypes.bool.isRequired,
+    likes: PropTypes.array.isRequired,
+    comments: PropTypes.array.isRequired,
+    dateCreated: PropTypes.number.isRequired
+  })
+};
