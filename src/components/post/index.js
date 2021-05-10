c46542615cd8003a526dbece1adaaa22346ff718
Chiara Campalani <campalanichiara@gmail.com>
2021-05-10 12:44:48 +0900

    created photo, actions and footer component in Post

--- src/components/post/index.js
+++ src/components/post/index.js
@@ -1,13 +1,28 @@
 import { useRef } from 'react';
 import PropTypes from 'prop-types';
 import Header from './header';
+import Image from './image';
+import Actions from './actions';
+import Footer from './footer';
 
 export default function Post({ content }) {
+  const commentInput = useRef(null);
+  const handleFocus = () => commentInput.current.focus();
+  // useRef to skip updating the state every time and rerendering the component
+
   // components:
-  // header, photo, tions (like & comment icons), footer, comment box
+  // header, photo, actions (like & comment icons), footer, comment box
   return (
     <div className="rounded col-span-4 bg-white border border-gray-primary mb-16">
       <Header username={content.username} />
+      <Image src={content.imageSrc} caption={content.caption} />
+      <Actions
+        docId={content.docId}
+        totalLikes={content.likes.length}
+        likedPhoto={content.userLikedPhoto}
+        handleFocus={handleFocus}
+      />
+      <Footer caption={content.caption} username={content.username} />
     </div>
   );
 }
